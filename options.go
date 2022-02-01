eddb1d900745e423531e4efc4876d651c76ded2b
filipecosta90 <filipecosta.90@gmail.com>
2022-02-01 09:36:13 +0000

    New sample feature

--- options.go
+++ options.go
@@ -5,6 +5,12 @@ type MyOptions struct {
 	v           int
 }
 
+// NewMyOptions returns a new MyOptions struct with default value.
+func NewMyOptionsDefault() *MyOptions {
+	return &MyOptions{}
+}
+
+
 // NewMyOptions returns a new MyOptions struct.
 func NewMyOptions(v int) *MyOptions {
 	return &MyOptions{v: v}
